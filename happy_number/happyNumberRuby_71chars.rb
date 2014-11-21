d2304bf69b0add683d9fee7627cabd7631e5da2d
René Helmke <rhelmke@uni-osnabrueck.de>
2014-11-21 22:46:05 +0100

    Fixed algorithm for case 0. Adds one symbol

--- happy_number/happyNumberRuby_71chars.rb
+++ happy_number/happyNumberRuby_71chars.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/ruby
 
 # @author Rene Helmke <rhelmke@uos.de>
-# Codegolf Happy Numbers in Ruby with 71 characters.
+# Codegolf Happy Numbers in Ruby with 72 characters.
 
-#   |---------------------------------------------------------------------|
-f = ->(n){while n>1&&n!=4;a,n=n,0;while a>0;n+=(a%10)**2;a/=10;end;end;n<2}
+#   |----------------------------------------------------------------------|
+f = ->(n){while n>1&&n!=4;a,n=n,0;while a>0;n+=(a%10)**2;a/=10;end;end;n==1}
 
 ####I think this version is prettier..unfortunately the rules dont allow it...
 #f = ->(n){while n>1&&n!=4;n=n.to_s.chars.reduce(0){|s,i|s+i.to_i**2}end;n<2}
 
 
 #output
-(1..100).each {|i| puts "%d -> %s" %[i, f.call(i).to_s]}
+(0..100).each {|i| puts "%d -> %s" %[i, f.call(i).to_s]}
