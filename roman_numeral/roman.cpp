44b66de05e94b27b17e97c398b876b6ab39629e1
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2014-12-01 21:07:42 +0100

    Fixed stupid bug... It should work now...

--- roman_numeral/roman.cpp
+++ roman_numeral/roman.cpp
@@ -6,9 +6,12 @@
 
 #include <iostream>
 using namespace std;
+#define x case
 
-// This thing is the complete function..  197 chars
-void f(int n,int o=0){auto p=[](int i){cout<<"IVXLCDM"[i];};if(n>9)f(n/10,o+2);n%=10;if((n-1)&4){p(o+1);n-=5;}p(o+0);switch(n){case 3:p(o+0);case 2:p(o+0);break;case 4:p(o+1);break;case 9:p(o+2);}}
+// This thing is the complete function..  215 chars
+void f(int n,int o=0){auto p=[](int i){cout<<"IVXLCDM"[i];};if(n>9)f(n/10,o+2);n%=10;s:if(n>0){if((n-1)&4){p(o+1);n-=5;goto s;}p(o+0);switch(n){case 3:p(o+0);case 2:p(o+0);break;case 4:p(o+1);break;case 9:p(o+2);}}}
+
+// void f(int n,int o=0){auto p=[](int i){cout<<"IVXLCDM"[i];};if(n>9)f(n/10,o+2);n%=10;if((n-1)&4){p(o+1);n-=5;}p(o+0);switch(n){case 3:p(o+0);case 2:p(o+0);break;case 4:p(o+1);break;case 9:p(o+2);}}
 
 void test(int num) {
   cout << num << " -> ";
@@ -20,8 +23,8 @@ int main() {
   // for(int i = 1; i < 4000; i++) {
   //   test(i);
   // }
-  test(3);
-  test(4);
-  test(8);
+  test(5);
+  test(10);
+  test(15);
   test(3999);
 }
